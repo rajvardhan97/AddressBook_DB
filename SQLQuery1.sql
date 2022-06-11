a4eab8241dcecff60dfd15599912bdf1865a71eb
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-11 15:56:55 +0530

    [Raj]Add: Create Address Book Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,11 @@
 create database AddressBook_Service
+create table AddressBook_Table
+(ID int IDENTITY(1,1) Primary Key,
+Firstame varchar(15)Not Null,
+Lastname varchar(15)Not Null,
+Address varchar(50)Not Null,
+City varchar(10)Not Null,
+State varchar(10) Not Null,
+Zip Bigint Not Null,
+PhoneNumber Bigint Not Null,
+Email varchar(20) Not Null)
