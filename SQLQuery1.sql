8a9ad661f6f98cdfe642ffbe6c220227869c874b
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-06-11 16:31:27 +0530

    [Raj]Add: Edit existing Contact person using their name

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@ create database AddressBook_Service
 
 create table AddressBook_Table
 (ID int IDENTITY(1,1) Primary Key,
-Firstame varchar(15)Not Null,
+Firstname varchar(15)Not Null,
 Lastname varchar(15)Not Null,
 Address varchar(50)Not Null,
 City varchar(15)Not Null,
@@ -11,8 +11,10 @@ Zip Bigint Not Null,
 PhoneNumber Bigint Not Null,
 Email varchar(50) Not Null)
 
-Insert into AddressBook_Table(Firstame, Lastname, Address, City, State, Zip, PhoneNumber, Email)
+Insert into AddressBook_Table(Firstname, Lastname, Address, City, State, Zip, PhoneNumber, Email)
 Values('Abhishekh', 'Ratha', 'Old Mussorie Road', 'Dehradun', 'Uttrakhand', 248001, 7017782365, 'abhirartha@gmail.com'),
 ('Kshitij', 'Puri', 'Turner Road', 'Roorke', 'Uttrakhand', 267001, 9837010277, 'puri.kshitij10@gmail.com'),
 ('Himanshu', 'Mahara', 'Sector-84','Noida', 'Uttar Pradesh', 247965, 8926452301, 'himanshu249@gmail.com')
+
+Update AddressBook_Table set PhoneNumber = 8006907440 where Firstname = 'Himanshu'
 
